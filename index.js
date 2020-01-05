c6ee23a956e8fb21503d4ec15d3cbbaa46c34dab
Dmitry Karyakin <skycrazyk@gmail.com>
2020-01-05 16:53:42 +0500

    added image handler

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 function getProducts(dataFiles) {
   return dataFiles.reduce((acc, fileName) => {
@@ -20,23 +21,42 @@ function getProducts(dataFiles) {
 
 http
   .createServer(function(req, res) {
-    const params = new URLSearchParams(req.url.slice(1));
-    const dealers = params.get('dealers');
+    const [urlPath, query] = req.url.split('?');
 
-    let products = [];
+    if (urlPath === '/') {
+      const params = new URLSearchParams(query);
+      const dealers = params.get('dealers');
 
-    if (dealers) {
-      // get particular dealers
-      const dataFiles = dealers.split(',').map(i => `${i}.json`);
-      products = getProducts(dataFiles);
+      let products = [];
+
+      if (dealers) {
+        // get particular dealers
+        const dataFiles = dealers.split(',').map(i => `${i}.json`);
+        products = getProducts(dataFiles);
+      } else {
+        // get all dealers
+        const dataFiles = fs.readdirSync('./data/');
+        products = getProducts(dataFiles);
+      }
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify(products));
+      res.end();
     } else {
-      // get all dealers
-      const dataFiles = fs.readdirSync('./data/');
-      products = getProducts(dataFiles);
+      fs.readFile(path.join(__dirname, 'public', urlPath), (err, content) => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            res.writeHead(404);
+            res.end();
+          } else {
+            throw err;
+          }
+        } else {
+          res.writeHead(200);
+          res.write(content);
+          res.end();
+        }
+      });
     }
-
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(products));
-    res.end();
   })
   .listen(8080);
